77ffe3edc06453479e5aa4e77a189df2bade3ac5
a325245 <61207845+a325245@users.noreply.github.com>
2026-04-19 21:27:34 -0500

    Delete ChatCasino directory
    re-upping in a sec

--- ChatCasino/Models/BaccaratBet.cs
+++
@@ -1,8 +0,0 @@
-namespace ChatCasino.Models;
-
-public class BaccaratBet
-{
-    public int PlayerBet { get; set; } = 0;
-    public int BankerBet { get; set; } = 0;
-    public int TieBet   { get; set; } = 0;
-}
--- ChatCasino/Models/ChocoboBet.cs
+++
@@ -1,7 +0,0 @@
-namespace ChatCasino.Models;
-
-public class ChocoboBet
-{
-    public int RacerIndex { get; set; }  // 0-based index into ChocoboEngine.Roster
-    public int Amount     { get; set; }
-}
