86e5d543827472d23e407419471f443bbf617ef5
a325245 <61207845+a325245@users.noreply.github.com>
2026-05-08 09:47:08 -0500

    Delete ChatCasino directory for a reupload

--- ChatCasino/Models/UltimaDeckCard.cs
+++
@@ -1,3 +0,0 @@
-namespace ChatCasino.Models;
-
-public readonly record struct UltimaDeckCard(string Color, string Type, string Code);
--- ChatCasino/Services/IDeckService.cs
+++
@@ -1,9 +0,0 @@
-using ChatCasino.Models;
-
-namespace ChatCasino.Services;
-
-public interface IDeckService
-{
-    DeckShoe<Card> GetStandardDeck(int deckCount, bool shuffled);
-    DeckShoe<UltimaDeckCard> GetUltimaDeck();
-}
